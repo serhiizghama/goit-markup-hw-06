800f674ac7d89f5da5b532c66c2889ec0ab599fa
serhii.zghama <serhii.zghama@edetek.com>
2024-07-17 17:22:43 +0200

    Init commit for Homework

---
+++ script.js
@@ -0,0 +1,9 @@
+function modalWindow() {
+  var modal = document.getElementById("modal-window");
+  modal.classList.toggle("is-open");
+}
+
+function mobileToggleMenu() {
+  var menu = document.getElementById("mobile-toggle-menu");
+  menu.classList.toggle("is-open");
+}
